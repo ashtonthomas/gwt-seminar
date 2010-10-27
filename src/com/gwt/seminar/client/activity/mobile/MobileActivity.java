03e12e8f8727256a0446535af9444bdf8a1fbc7c
Ashton Thomas <ashton@acrinta.com>
2010-10-27 16:35:04 -0400

    adding a package for mobile activities and a mobile interface for activities

---
+++ src/com/gwt/seminar/client/activity/mobile/MobileActivity.java
@@ -0,0 +1,13 @@
+package com.gwt.seminar.client.activity.mobile;
+
+import com.google.gwt.place.shared.Place;
+
+public interface MobileActivity {
+	/* using Spring Roo autogerated code as a template 
+	 * As I am in many areas of this application */
+	Place getBackButtonPlace();
+	
+	String getBackButtonText();
+	
+	String getTitleText();
+}
